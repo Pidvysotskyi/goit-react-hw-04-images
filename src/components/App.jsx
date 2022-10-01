d926b85dbe8f1e05613636226863884dfb555bda
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2022-10-01 18:55:54 +0300

    basic functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,61 @@ import { InfinitySpin as Loader } from 'react-loader-spinner';
 import { Container } from './App.styled';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
+import { Button } from './Button/Button';
+import { getImages } from 'utils/getImages';
 
 export class App extends Component {
   state = {
     query: '',
     pageNumber: 1,
     status: 'idle',
+    images: [],
+    isModal: false,
   };
+  componentDidUpdate(_, prevState) {
+    const currentQuery = this.state.query;
+    const currentPage = this.state.pageNumber;
+    if (prevState.query !== currentQuery) {
+      this.setState({ pageNumber: 1, status: 'pending' });
+      getImages(currentQuery, currentPage).then(response => {
+        this.setState({ images: response, status: 'done' });
+      });
+    } else if (prevState.pageNumber !== currentPage) {
+      this.setState({ status: 'pending' });
+      getImages(currentQuery, currentPage).then(response => {
+        this.setState(prevState => {
+          return {
+            images: [...prevState.images, ...response],
+            status: 'done',
+          };
+        });
+      });
+    }
+  }
+
   onSearchHandle = value => {
     this.setState({ query: value });
+  };
+
+  onLoadMoreHandle = () => {
+    this.setState(prevState => {
+      return { pageNumber: prevState.pageNumber + 1 };
+    });
   };
 
   render() {
     return (
       <Container>
         <Searchbar onSearch={this.onSearchHandle} />
-        {this.state.query !== '' && (
-          <ImageGallery
-            query={this.state.query}
-            pageNumber={this.state.pageNumber}
-          />
+        {this.state.status === 'done' && (
+          <ImageGallery images={this.state.images} />
         )}
-        <Loader width="200" color="#4fa94d" />
+        {this.state.status === 'done' && (
+          <Button onClick={this.onLoadMoreHandle}>Load more</Button>
+        )}
+        {this.state.status === 'pending' && (
+          <Loader width="200" color="#4fa94d" />
+        )}
       </Container>
     );
   }
---
+++ src/utils/getImages.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API_KEY = '29264907-2ab68c5d7b62ca0acfac904a2';
+
+export function getImages(query, pageNumber) {
+  return axios
+    .get(
+      `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+    .then(response => response.data.hits)
+    .then(hits => {
+      return hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+        return { id, webformatURL, largeImageURL, tags };
+      });
+    });
+}
